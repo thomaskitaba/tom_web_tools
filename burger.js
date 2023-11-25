2bbeeebeb84f68dd8e7cdaa2b5106d6a05b075dc
thomaskitaba <thomaskitaba@gmail.com>
2023-11-25 20:26:13 +0300

    recorected_corrected_humberger_menu_and_corrected_jquery_for_it

--- burger.js
+++ burger.js
@@ -4,17 +4,18 @@ $(document).ready(function () {
   // toggle ".nav_bar' between display flex and none
   // when the burger icon is clicked
 
-  $('.burger').click(function () {
-    if (clickStatus === 0) {
-      $('.nav_bar').css('display', 'flex');
 
-      clickStatus = 1;
-    } else {
-      $('.nav_bar').css('display', 'none');
-      clickStatus = 0;
-    }
-  });
-  if (windows_width <= 800) {
+  if (windows_width < 800) {
+    $('.burger').click(function () {
+        if (clickStatus === 0) {
+          $('.nav_bar').css('display', 'flex');
+
+          clickStatus = 1;
+        } else {
+          $('.nav_bar').css('display', 'none');
+          clickStatus = 0;
+        }
+      });
     $(document).click(function (e) {
       if (!$(e.target).closest('.nav_bar').length && !$(e.target).closest('.burger').length) {
         // If the clicked element is neither .nav_bar nor .burger, hide .nav_bar
