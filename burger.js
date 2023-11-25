79865b6bed586b78137f292dca0b938723e3cbad
thomaskitaba <thomaskitaba@gmail.com>
2023-11-25 20:35:36 +0300

    responsive  working_burger_menu

--- burger.js
+++ burger.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-  let clickStatus = 0;
-  const windows_width = $(windows).width();
-  // toggle ".nav_bar' between display flex and none
-  // when the burger icon is clicked
+    let clickStatus = 0;
+    var windowWidth = $(window).width();
+    // toggle ".nav_bar' between display flex and none
+    // when the burger icon is clicked
 
-
-  if (windows_width < 800) {
     $('.burger').click(function () {
         if (clickStatus === 0) {
           $('.nav_bar').css('display', 'flex');
@@ -16,22 +14,24 @@ $(document).ready(function () {
           clickStatus = 0;
         }
       });
-    $(document).click(function (e) {
-      if (!$(e.target).closest('.nav_bar').length && !$(e.target).closest('.burger').length) {
-        // If the clicked element is neither .nav_bar nor .burger, hide .nav_bar
-        $('.nav_bar').css('display', 'none');
-        toggle_click = 1;
-        clickStatus = 0;
-      }
+      if (windowWidth <= 769)
+      {
+        $(document).click(function (e) {
+        if (!$(e.target).closest('.nav_bar').length && !$(e.target).closest('.burger').length) {
+          // If the clicked element is neither .nav_bar nor .burger, hide .nav_bar
+          $('.nav_bar').css('display', 'none');
+          clickStatus = 0;
+        }
+      });
+    }
+    if (windowWidth > 769)
+    {
+        $(".nav_bar").css('display', 'flex');
+    }
+    // add hover effect to the burger menu itself
+    $('.burger').hover(function(){
+        $(".burger").addClass("burger_highlight");
+    }, function(){$(".burger").removeClass("burger_highlight");
+        $(".nav_bard").css("display", "none");
     });
-  } else {
-    $('.nav_bar').css('display', 'flex');
-  }
-  // add hover effect to the burger menu itself
-  $('.burger').hover(function () {
-    $('.burger').addClass('burger_highlight');
-  }, function () {
-    $('.burger').removeClass('burger_highlight');
-    $('.nav_bard').css('display', 'none');
   });
-});
