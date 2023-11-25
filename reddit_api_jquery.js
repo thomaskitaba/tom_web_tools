1e26c3a11d75a8bcc1d0d4198d2c7493c67da731
thomaskitaba <thomaskitaba@gmail.com>
2023-11-25 12:31:08 +0300

    update

--- reddit_api_jquery.js
+++ reddit_api_jquery.js
@@ -1,18 +1,15 @@
-$(document).ready(function() {
-    // Get the value from the text box
+$(document).ready(function () {
+  // Get the value from the text box
 
+  $('#submitButtonGet').click(function () {
+    const subreddit = $('#myTextbox').val();
+    // https://www.reddit.com/r/{subreddit}/hot.json
+    const endpoint1 = 'https://www.reddit.com/r/' + subreddit + '/hot.json';
+    $('#resultGet').text(endpoint1);
+    $.get(endpoint1, function (response) {
+      // Display the value
 
-    $("#submitButtonGet").click(function() {
-
-        var subreddit = $("#myTextbox").val();
-        // https://www.reddit.com/r/{subreddit}/hot.json
-        endpoint1 = "https://www.reddit.com/r/" + subreddit + "/hot.json";
-        $("#resultGet").text(endpoint1);
-        $.get(endpoint1,  function(response){
-        // Display the value
-
-          $("#resultGet").text(JSON.stringify(response, null, 2));
-
-        });
-     });
+      $('#resultGet').text(JSON.stringify(response, null, 2));
+    });
+  });
 });
